d09368f28a282ff22813873d9261848467098d74
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-07-11 14:47:55 -0300

    Projetos Pessoais

---
+++ calculaaltura/script.js
@@ -0,0 +1,25 @@
+const alturaPai = document.querySelector('#paitxt');
+const alturaMae = document.querySelector('#maetxt');
+const res = document.querySelector('#resultado');
+
+
+function homem() {
+    let resultadoM = (Number(alturaPai.value) + Number(alturaMae.value)) / 2 + 6.5; 
+    res.innerHTML = `Seu filho terá ${resultadoM}cm`
+    if (alturaMae.value == 0 || alturaPai.value == 0) {
+        res.innerHTML = ' <strong> Erro</strong>. Altura do <span class = "pai"> PAI </span>ou<span class = "mae"> MÃE </span>inválidas.';
+    }
+}
+
+function mulher() {
+    let resultadoF = (Number(alturaPai.value) + Number(alturaMae.value)) / 2 - 6.5;
+    res.innerHTML = `Sua filha terá ${resultadoF}cm.`
+    if (alturaPai.value == 0 || alturaMae.value == 0) {
+        res.innerHTML = ' <strong>Erro</strong>. Altura do <span class = "pai"> PAI </span>ou<span class = "mae"> MÃE </span>inválidas.';
+    }
+    
+    
+    
+}
+
+
