9e0be55a013d667107ada811eb440fe9325afa74
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-09-25 20:50:54 -0300

    menun animado inicio

---
+++ menu-animado/script.js
@@ -0,0 +1,11 @@
+const botao = document.querySelector('.botao-menu')
+const menuLateral = document.querySelector('.menu-lateral')
+const conteudo = document.querySelector('.conteudo')
+const background = document.querySelector('.background')
+
+
+botao.addEventListener('click', () => {
+    console.log('opa');
+    
+
+})
