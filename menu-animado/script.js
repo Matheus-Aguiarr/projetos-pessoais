77a1c76e56a8b374bb37c32a25f54e176bb5c37a
Matheus Aguiar <matheusaguiardealmeida@gmail.com>
2024-09-26 17:47:38 -0300

    menu-animado finalizado

--- menu-animado/script.js
+++ menu-animado/script.js
@@ -1,11 +1,21 @@
-const botao = document.querySelector('.botao-menu')
-const menuLateral = document.querySelector('.menu-lateral')
-const conteudo = document.querySelector('.conteudo')
-const background = document.querySelector('.background')
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".botao-menu");
+    const menuLateral = document.querySelector('.menu-lateral');
+    const conteudo = document.querySelector('.conteudo');
+    const background = document.querySelector('.background');
 
 
-botao.addEventListener('click', () => {
-    console.log('opa');
+
     
+    button.addEventListener('click', () => {
+        menuLateral.classList.toggle('ativo');
+        conteudo.classList.toggle('ativo');
+        button.classList.toggle('ativo');
+        background.classList.toggle('ativo')
+        document.body.style.backgroundColor = menuLateral.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
 
-})
+      
+    })
+
+    
+});
